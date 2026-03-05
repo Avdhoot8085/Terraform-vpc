a3f2dcc8e866b5aab45e407912ace7654a9d1f7a
avdhoot <avdhootlad4782@gmail.com>
2026-03-05 06:41:50 +0530

    terraform is ready

--- vpc.tf
+++ vpc.tf
@@ -9,11 +9,50 @@ resource "aws_vpc" "main" {
     Name = "my-vpc"
   }
 }
-resource "aws_subnet" "subnet" {
+
+resource "aws_subnet" "subnet_1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/20"
+  availability_zone = "ap-south-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "my-subnet-1"
   }
+}
+
+resource "aws_subnet" "subnet_2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.16.0/20"
+
+  tags = {
+    Name = "my-subnet-2"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "my-IG"
+  }
+}
+
+resource "aws_route_table" "route" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+    Name = "my-route_table"
+  }
+  
+}
+
+resource "aws_route" "public_route" {
+    route_table_id = aws_route_table.route.id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id 
+}
+
+resource "aws_route_table_association" "associate" {
+  subnet_id      = aws_subnet.subnet_1.id
+  route_table_id = aws_route_table.route.id
 }
